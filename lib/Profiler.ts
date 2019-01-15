c4e7ab5f81e7a400b90b263690cdfff0b3161c59
Stian Bakkane <stianba@gmail.com>
2019-01-15 12:50:52 +0100

    back to cors

--- lib/Profiler.ts
+++ lib/Profiler.ts
@@ -81,9 +81,7 @@ class Profiler {
 
       const response = await fetch(url, {
         method: 'POST',
-        mode: 'no-cors',
         cache: 'no-cache',
-        credentials: 'omit',
         headers: {
           'Content-Type': 'application/json'
         },
