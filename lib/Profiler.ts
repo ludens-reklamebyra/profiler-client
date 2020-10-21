949bfcd662c6f172aa1cf38734d5065050a0317b
Stian Bakkane <stianba@gmail.com>
2020-10-21 08:47:20 +0200

    Added cookie consent mode

--- lib/Profiler.ts
+++ lib/Profiler.ts
@@ -1,11 +1,15 @@
-import * as qs from 'qs';
-import * as cookies from 'js-cookie';
+import * as qs from "qs";
+import * as cookies from "js-cookie";
+
+type ConsentService = "cookiebot";
 
 interface Opts {
   organization: string;
   personalize?: boolean;
   dataPointSetDelay?: number;
   contactEmail?: string;
+  hasConsent?: boolean;
+  consentService?: ConsentService;
 }
 
 interface UpdateProfileOpts {
@@ -86,14 +90,15 @@ interface PageView {
   exit?: string;
 }
 
-const PROFILER_URL = 'https://api.profiler.marketing';
-const PERSONALIZATION_CLASS_NAME = '__prfPrs';
-const COOKIE_PID_KEY = '__pid';
-const COOKIE_SID_KEY = '__psid';
+const PROFILER_URL = "https://api.profiler.marketing";
+const PERSONALIZATION_CLASS_NAME = "__prfPrs";
+const COOKIE_PID_KEY = "__pid";
+const COOKIE_SID_KEY = "__psid";
 const DEFAULT_DP_DELAY = 10000;
 
 class Profiler {
   private organization: string;
+  private hasConsent: boolean;
   private personalize: boolean;
   private dpDelay: number;
   private pid?: string;
@@ -101,12 +106,16 @@ class Profiler {
   private pageView?: PageView;
   private dpDelayTimerId?: number;
   private contactEmail?: string;
+  private consentService?: ConsentService;
 
   constructor(opts: Opts) {
     this.organization = opts.organization;
+    this.hasConsent = opts.hasConsent || false;
     this.personalize = opts.personalize || false;
     this.dpDelay = opts.dataPointSetDelay || DEFAULT_DP_DELAY;
     this.contactEmail = opts.contactEmail;
+    this.consentService = opts.consentService;
+    this.readConsent();
     this.readPidFromCookie();
     this.readSidFromCookie();
     this.handlePersonalizations();
@@ -117,7 +126,7 @@ class Profiler {
 
   public async updateProfile(opts: UpdateProfileOpts) {
     try {
-      const endpoint = 'contacts/update';
+      const endpoint = "contacts/update";
 
       await this.network(endpoint, opts.data, true);
 
@@ -130,15 +139,15 @@ class Profiler {
   public async pushDataPoint(opts: PushDataPointOpts) {
     try {
       const endpoint =
-        'organizations/' +
+        "organizations/" +
         this.organization +
-        '/data-points/' +
+        "/data-points/" +
         opts.dataPoint +
-        '/set';
+        "/set";
 
       await this.network(endpoint, {
         value: opts.value,
-        email: opts.contactEmail || this.contactEmail
+        email: opts.contactEmail || this.contactEmail,
       });
 
       this.handlePersonalizations();
@@ -154,16 +163,16 @@ class Profiler {
       for (let i = 0; i < opts.dataPoints.length; i++) {
         const dp = opts.dataPoints[i];
         const endpoint =
-          'organizations/' +
+          "organizations/" +
           this.organization +
-          '/data-points/' +
+          "/data-points/" +
           dp.dataPoint +
-          '/set';
+          "/set";
 
         promises.push(
           this.network(endpoint, {
             value: dp.value,
-            email: opts.contactEmail || this.contactEmail
+            email: opts.contactEmail || this.contactEmail,
           })
         );
       }
@@ -177,7 +186,7 @@ class Profiler {
 
   public async pushAction(opts: PushActionOpts) {
     try {
-      const endpoint = 'organizations/' + this.organization + '/actions/push';
+      const endpoint = "organizations/" + this.organization + "/actions/push";
 
       await this.network(
         endpoint,
@@ -187,7 +196,7 @@ class Profiler {
           label: opts.label,
           value: opts.value,
           email: opts.contactEmail || this.contactEmail,
-          contactData: opts.contactData
+          contactData: opts.contactData,
         },
         true
       );
@@ -202,14 +211,14 @@ class Profiler {
     try {
       const query = {
         organization: this.organization,
-        ref: this.pid
+        ref: this.pid,
       };
 
       const response = await fetch(
         `${PROFILER_URL}/personalization?${qs.stringify(query)}`,
         {
-          mode: 'cors',
-          credentials: 'include'
+          mode: "cors",
+          credentials: "include",
         }
       );
 
@@ -229,28 +238,28 @@ class Profiler {
     try {
       if (
         document &&
-        'referrer' in document &&
+        "referrer" in document &&
         window &&
-        'location' in window &&
+        "location" in window &&
         !this.sid
       ) {
         const firstParty = window.location.href;
         const thirdParty = document.referrer;
 
         const json = await this.network(
-          'contacts/new-session',
+          "contacts/new-session",
           {
             organization: this.organization,
             email: this.contactEmail,
             firstParty,
-            thirdParty
+            thirdParty,
           },
           true
         );
 
         this.handlePersonalizations();
 
-        if (json && 'sessionId' in json) {
+        if (json && "sessionId" in json) {
           this.setSid(json.sessionId);
         }
       }
@@ -261,11 +270,11 @@ class Profiler {
 
   public async collect(opts: CollectOpts): Promise<Response> {
     return await this.network(
-      'campaigns/' +
+      "campaigns/" +
         opts.campaign +
-        '/collectors/' +
+        "/collectors/" +
         opts.collector +
-        '/collect',
+        "/collect",
       opts.data,
       true
     );
@@ -273,17 +282,17 @@ class Profiler {
 
   public readMeta() {
     if (window && document) {
-      const metas = document.getElementsByTagName('meta');
+      const metas = document.getElementsByTagName("meta");
       const dpsToPush: DataPoint[] = [];
 
       for (let i = 0; i < metas.length; i++) {
         const meta = metas[i];
 
-        if (meta.getAttribute('name') === 'profiler:interests') {
-          const contents = (meta.getAttribute('content') || '').split(',');
+        if (meta.getAttribute("name") === "profiler:interests") {
+          const contents = (meta.getAttribute("content") || "").split(",");
 
           for (let j = 0; j < contents.length; j++) {
-            const contentArr = contents[j].split(':');
+            const contentArr = contents[j].split(":");
 
             if (contentArr.length > 0) {
               const ref = contentArr[0];
@@ -292,7 +301,7 @@ class Profiler {
 
               dpsToPush.push({
                 dataPoint: ref,
-                value: weight
+                value: weight,
               });
             }
           }
@@ -314,7 +323,7 @@ class Profiler {
 
     this.pageView = {
       url: !!opts && !!opts.url ? opts.url : window.location.href,
-      enter: new Date().toISOString()
+      enter: new Date().toISOString(),
     };
 
     if (!!this.dpDelayTimerId && window) {
@@ -326,7 +335,7 @@ class Profiler {
 
   private async handlePersonalizations() {
     try {
-      if (!this.personalize) {
+      if (!this.hasConsent || !this.personalize) {
         return;
       }
 
@@ -340,18 +349,18 @@ class Profiler {
 
           if (ps.html) {
             const elems = document.querySelectorAll(
-              ps.htmlQuerySelector ? ps.htmlQuerySelector : 'body'
+              ps.htmlQuerySelector ? ps.htmlQuerySelector : "body"
             );
 
             for (let j = 0; j < elems.length; j++) {
               const elem = elems[j];
 
-              if (ps.htmlPlacement === 'replace') {
+              if (ps.htmlPlacement === "replace") {
                 elem.innerHTML = this.wrapDom(ps.html);
               } else {
                 elem.insertAdjacentHTML(
                   //@ts-ignore How to fix this?
-                  ps.htmlPlacement || 'beforeend',
+                  ps.htmlPlacement || "beforeend",
                   this.wrapDom(ps.html)
                 );
               }
@@ -359,7 +368,7 @@ class Profiler {
           }
 
           if (ps.js) {
-            const scriptTag = document.createElement('script');
+            const scriptTag = document.createElement("script");
             scriptTag.innerHTML = ps.js;
             document.body.appendChild(scriptTag);
           }
@@ -375,8 +384,12 @@ class Profiler {
     data: RequestData,
     asJSON?: boolean
   ): Promise<any> {
-    if (typeof this.pid === 'string') {
-      data['ref'] = this.pid;
+    if (!this.hasConsent) {
+      return;
+    }
+
+    if (typeof this.pid === "string") {
+      data["ref"] = this.pid;
     }
 
     let url = `${PROFILER_URL}/${endpoint}`;
@@ -386,18 +399,18 @@ class Profiler {
     }
 
     const response = await fetch(url, {
-      method: 'POST',
-      mode: 'cors',
-      credentials: 'include',
+      method: "POST",
+      mode: "cors",
+      credentials: "include",
       headers: {
-        'Content-Type': 'application/json'
+        "Content-Type": "application/json",
       },
-      body: asJSON ? JSON.stringify(data) : null
+      body: asJSON ? JSON.stringify(data) : null,
     });
 
     const responseJSON = await response.json();
 
-    if (!!responseJSON && 'ref' in responseJSON) {
+    if (!!responseJSON && "ref" in responseJSON) {
       this.setPid(responseJSON.ref as string);
     }
 
@@ -406,7 +419,7 @@ class Profiler {
 
   private wrapDom(html: string): string {
     return (
-      '<span class="' + PERSONALIZATION_CLASS_NAME + '">' + html + '</span>'
+      '<span class="' + PERSONALIZATION_CLASS_NAME + '">' + html + "</span>"
     );
   }
 
@@ -426,7 +439,7 @@ class Profiler {
       this.pid = pid;
 
       cookies.set(COOKIE_PID_KEY, pid, {
-        expires: 365
+        expires: 365,
       });
     }
   }
@@ -439,10 +452,28 @@ class Profiler {
     }
   }
 
+  private readConsent() {
+    try {
+      if (this.consentService === "cookiebot") {
+        const cookie = cookies.get("CookieConsent");
+
+        if (cookie) {
+          const data = JSON.parse(decodeURIComponent(cookie));
+
+          if (data.marketing === true) {
+            this.hasConsent = true;
+          }
+        }
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
   private readPidFromCookie() {
     const pid = cookies.get(COOKIE_PID_KEY);
 
-    if (typeof pid === 'string') {
+    if (typeof pid === "string") {
       this.pid = pid;
     }
   }
@@ -450,7 +481,7 @@ class Profiler {
   private readSidFromCookie() {
     const sid = cookies.get(COOKIE_SID_KEY);
 
-    if (typeof sid === 'string') {
+    if (typeof sid === "string") {
       this.sid = sid;
     }
   }
@@ -461,21 +492,21 @@ class Profiler {
 
       if (
         !!navigator &&
-        'sendBeacon' in navigator &&
-        typeof this.pid === 'string'
+        "sendBeacon" in navigator &&
+        typeof this.pid === "string"
       ) {
         const endpoint =
           PROFILER_URL +
-          '/organizations/' +
+          "/organizations/" +
           this.organization +
-          '/page-views/push';
+          "/page-views/push";
 
         const data = new FormData();
 
-        data.append('ref', this.pid);
+        data.append("ref", this.pid);
 
         if (this.sid) {
-          data.append('sessionId', this.sid);
+          data.append("sessionId", this.sid);
         }
 
         for (const key in this.pageView) {
@@ -492,8 +523,8 @@ class Profiler {
   }
 
   private listenForPageUnload() {
-    if (window && 'addEventListener' in window) {
-      window.addEventListener('unload', () => this.endPageView());
+    if (window && "addEventListener" in window) {
+      window.addEventListener("unload", () => this.endPageView());
     }
   }
 }
